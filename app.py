0a00cdd1abfd7fca7b0e1a13b5fed45e95e4567a
Rose1445 <43977961+Rose1445@users.noreply.github.com>
2018-10-24 16:36:01 +0800

    S06210045

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def handle_message(event):
     message = TextSendMessage(text='Nice to meet you!')
     message2 = TextSendMessage(text=(event.source.user_id)) #reply userid
     line_bot_api.reply_message(event.reply_token, message2)
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
